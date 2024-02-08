d51ca9953632be53e6136919a433f35e9443723b
Anil <70906059+anil-jpg@users.noreply.github.com>
2024-02-08 18:52:39 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "sairam" {
+resource "aws_instance" "saisrinath" {
   ami           = "ami-0277155c3f0ab2930"
   instance_type = "t2.miro"
   tags = {
