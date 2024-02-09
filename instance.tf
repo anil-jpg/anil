df70900af6110c3329ba428682928796f0bf0c61
Anil <70906059+anil-jpg@users.noreply.github.com>
2024-02-09 12:35:26 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,11 +2,11 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "anilkumar" {
+resource "aws_instance" "nonprod" {
   ami           = "ami-0a3c3a20c09d6f377"
   instance_type = "t2.micro"
 
   tags = {
-  Name = "anilkumar"
+  Name = "nonprod"
   }
 }
