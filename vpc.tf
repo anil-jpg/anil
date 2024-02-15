37061d22765d4fa89882b45d28d4a912c0b9ea33
Anil <70906059+anil-jpg@users.noreply.github.com>
2024-02-15 20:33:48 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,10 +2,11 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
+resource "aws_s3_bucket" "sairamsai" {
+  bucket = "my-tf-test-bucket"
 
   tags = {
-    Name = "main"
+    Name        = "sairamsai "
+    Environment = "Dev"
   }
 }
