d59607d77e2c8f6877c2d47e13609c024aeaf6d9
Anil <70906059+anil-jpg@users.noreply.github.com>
2024-02-16 07:57:30 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,10 +2,14 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "ramsainath" {
-  bucket = "ramsainath"
-
-  tags = {
-    Name        = "jairam"
-  }
+resource "aws_db_instance" "default" {
+  allocated_storage    = 400
+  db_name              = "mydatabase"
+  engine               = "MYSQL"
+  engine_version       = "8.0.35"
+  instance_class       = "db.m5d.xlarge"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
 }
