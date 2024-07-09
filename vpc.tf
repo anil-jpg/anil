90250015f3addf9ab85191cdd877169f5d35af69
Anil <70906059+anil-jpg@users.noreply.github.com>
2024-07-09 23:04:12 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_security_group" "sairam" {
   name        = "sairam"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = "vpc-04d475421beab95ed"
+  vpc_id      = "vpc-09e9c4d95ac72fe57"
 
   tags = {
     Name = "ramsai"
